58b328eb65072428285065c05acdeaa4a84c18f9
Capyyyyy <capybutworkemail@gmail.com>
2025-05-23 19:14:51 +0300

    added header (shocking)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<header>
+	<h1>Burned Chicken Demonlist</h1>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="/contact">Contact</a>
+	</nav>
+</header>
+
+<style>
+	header {
+		background-color: #333;
+		color: #fff;
+		padding: 1rem;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	h1 {
+		margin: 0;
+	}
+
+	nav {
+		display: flex;
+		gap: 1rem;
+	}
+
+	nav a {
+		color: #fff;
+		text-decoration: none;
+	}
+
+	nav a:hover {
+		text-decoration: underline;
+	}
+</style>
